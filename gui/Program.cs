a3358deced796c45c7188e7454a17151c1cf6261
ProxyBridge Team <proxybridge@interceptsuite.com>
2026-01-27 01:27:15 +0300

    Исправлено: убрана консоль отладки
    - Удален AllocConsole() из Program.cs
    - Теперь консоль не показывается совсем
    - Чистый запуск только GUI

--- gui/Program.cs
+++ gui/Program.cs
@@ -1,24 +1,13 @@
 using Avalonia;
 using System;
-using System.Runtime.InteropServices;
 
 namespace ProxyBridge.GUI;
 
 class Program
 {
-    [DllImport("kernel32.dll", SetLastError = true)]
-    [return: MarshalAs(UnmanagedType.Bool)]
-    static extern bool AllocConsole();
-
     [STAThread]
     public static void Main(string[] args)
     {
-        // Открываем консоль для логов
-        AllocConsole();
-        Console.WriteLine("=== ProxyBridge Debug Console ===");
-        Console.WriteLine("Time: " + DateTime.Now);
-        Console.WriteLine("================================\n");
-
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
     }
